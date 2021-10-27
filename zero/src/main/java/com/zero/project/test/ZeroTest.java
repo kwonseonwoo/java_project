eb39eecc360f69fbc691a1b5570966c2b9659378
yongjuvv <o01071767279@nate.com>
2021-10-27 14:28:08 +0900

    Update zero

--- zero/src/main/java/com/zero/project/test/ZeroTest.java
+++ zero/src/main/java/com/zero/project/test/ZeroTest.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class ZeroTest {
-//http://localhost:8080/zero/test/hello
+//http://localhost:8000/zero/test/hello
 	@GetMapping("/test/hello")
 	public String hello() {
 		return "<h1>hello spring boot</h1>";
