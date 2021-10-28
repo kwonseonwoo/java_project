c9bd0c4d797dc716f77be24b0285caaa653d564c
kwonseonwoo <kwon3152@naver.com>
2021-10-28 17:58:40 +0900

    admin test

---
+++ zero/src/main/java/com/zero/project/controller/MainController.java
@@ -0,0 +1,13 @@
+package com.zero.project.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class MainController {
+
+	@RequestMapping("/main")
+	public void main() {
+		
+	}
+}
---
+++ zero/src/main/java/com/zero/project/service/AdminMemberService.java
@@ -0,0 +1,24 @@
+package com.zero.project.service;
+
+import java.util.List;
+
+
+import com.zero.project.model.Criteria;
+import com.zero.project.model.MemberVO;
+
+
+public interface AdminMemberService {
+	
+	public MemberVO getMember(int member_no);
+	
+	public boolean modify(MemberVO vo);
+	
+	public boolean remove(int member_no);
+	
+	public List<MemberVO> getList();
+	
+	public List<MemberVO> getListWithPaging(Criteria cri);
+	
+	public int getTotalCount(Criteria cri);
+	
+}
